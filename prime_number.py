d406bf6d6988be5e5c47331ca3ce67ca6be64864
TakatoSakurai <saku21saku.06@gmail.com>
2023-05-30 22:32:24 +0900

    制御構文やりました

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,12 @@
 a = input("aの値を入力: ")
 b = input("bの値を入力: ")
-
-# TODO
+a = int(a)
+i = a + 1
+for j in range(2, i):
+    if a % j != 0:
+        continue
+    if j == a:
+         print(str(a) + 'は素数です')
+    if j != a:
+        print(str(a) + 'は素数でない')
+        break
